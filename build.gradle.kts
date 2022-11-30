a76e0cd1a28fe211959f418232b7f7f8b1acce3e
daniilsjb <daniilb@outlook.com>
2022-11-30 21:20:12 +0200

    Update README

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
-    application
 }
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -22,8 +18,4 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-application {
-    mainClass.set("MainKt")
 }
